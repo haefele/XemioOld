96226cc2cb720beb10f19e3c8364fc2d47c3e80d
Daniel Häfele <haefele@xemio.net>
2016-11-30 11:08:07 +0100

    Fix an issue in the FolderToFolderDTOMapper that would miss the parentFolderId

--- src/Server/Xemio.Server.Infrastructure/Mapping/FolderToFolderDTOMapper.cs
+++ src/Server/Xemio.Server.Infrastructure/Mapping/FolderToFolderDTOMapper.cs
@@ -5,6 +5,7 @@ using Xemio.Server.Contracts.Mapping;
 using System.Threading.Tasks;
 using Google.Protobuf;
 using EnsureThat;
+using Xemio.Server.Infrastructure.Database;
 
 namespace Xemio.Server.Infrastructure.Mapping
 {
@@ -12,18 +13,25 @@ namespace Xemio.Server.Infrastructure.Mapping
     public class FolderToFolderDTOMapper : MapperBase<Folder, FolderDTO>
     {
         private readonly IMapper<Guid?, string> _guidToStringMapper;
+        private readonly XemioContext _xemioContext;
 
-        public FolderToFolderDTOMapper(IMapper<Guid?, string> guidToStringMapper)
+        public FolderToFolderDTOMapper(IMapper<Guid?, string> guidToStringMapper, XemioContext xemioContext)
         {
             EnsureArg.IsNotNull(guidToStringMapper, nameof(guidToStringMapper));
+            EnsureArg.IsNotNull(xemioContext, nameof(xemioContext));
 
             this._guidToStringMapper = guidToStringMapper;
+            this._xemioContext = xemioContext;
         }
 
         public override async Task<FolderDTO> MapAsync(Folder input)
         {
             if (input == null)
                 return null;
+
+            await this._xemioContext.Entry(input)
+                .Reference(F => F.ParentFolder)
+                .LoadAsync();
 
             return new FolderDTO
             {
