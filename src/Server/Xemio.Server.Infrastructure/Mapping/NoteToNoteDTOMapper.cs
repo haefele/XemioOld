423ad5829c1ee2ba88658cb2014d02d026ab13bd
Daniel Häfele <haefele@xemio.net>
2016-12-18 22:39:12 +0100

    Add NoteDTO and a mapper for Note->NoteDTO

---
+++ src/Server/Xemio.Server.Infrastructure/Mapping/NoteToNoteDTOMapper.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using EnsureThat;
+using Xemio.Server.Contracts.Mapping;
+using Xemio.Server.Infrastructure.Database;
+using Xemio.Server.Infrastructure.Entities.Notes;
+using Xemio.Shared.Models.Notes;
+
+namespace Xemio.Server.Infrastructure.Mapping
+{
+    public class NoteToNoteDTOMapper : MapperBase<Note, NoteDTO>
+    {
+        private readonly XemioContext _xemioContext;
+
+        public NoteToNoteDTOMapper(XemioContext xemioContext)
+        {
+            EnsureArg.IsNotNull(xemioContext, nameof(xemioContext));
+
+            this._xemioContext = xemioContext;
+        }
+
+        public override async Task<NoteDTO> MapAsync(Note input)
+        {
+            if (input == null)
+                return null;
+
+            await this._xemioContext.Entry(input)
+                .Reference(f => f.Folder)
+                .LoadAsync();
+
+            return new NoteDTO
+            {
+                Id = input.Id,
+                Title = input.Title,
+                Content = input.Content,
+                ETag = input.ETag,
+                FolderId = input.Folder.Id,
+                FolderName = input.Folder.Name
+            };
+        }
+    }
+}
---
+++ src/Shared/Xemio.Shared/Models/Notes/NoteDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Xemio.Shared.Models.Notes
+{
+    public class NoteDTO
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Guid FolderId { get; set; }
+        public byte[] ETag { get; set; }
+        public string FolderName { get; set; }
+    }
+}
